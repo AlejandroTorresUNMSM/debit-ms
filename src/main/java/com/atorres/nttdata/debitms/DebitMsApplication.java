f250b2d6526fbf5199ca8d8b2b00cf846381194a
AlejandroTorresUNMSM <alephdmc2575@gmail.com>
2023-07-23 17:02:25 -0500

    dev: commit inicial

---
+++ src/main/java/com/atorres/nttdata/debitms/DebitMsApplication.java
@@ -0,0 +1,13 @@
+package com.atorres.nttdata.debitms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebitMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebitMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atorres/nttdata/debitms/DebitMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atorres.nttdata.debitms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebitMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
